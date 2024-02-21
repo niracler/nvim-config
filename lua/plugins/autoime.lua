00ee00995db26ec57825fcdfb754c1698b7012c3
niracler <i@niracler.com>
2024-02-21 11:13:03 +0800

    docs: add im-select

---
+++ lua/plugins/autoime.lua
@@ -0,0 +1,8 @@
+return {
+  "OliverChao/autoime.nvim",
+  event = "InsertEnter",
+  opts = {
+    im_select_path = "/opt/homebrew/bin/im-select",
+    autoime_default = "com.apple.keylayout.ABC", -- choose the default ime
+  },
+}
